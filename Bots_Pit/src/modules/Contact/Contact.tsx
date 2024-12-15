16c0e71481284a9d053bd630009a533e49c13c35
terracodum <nikitakharlov007@gmail.com>
2024-12-15 16:19:01 +0600

    feat: add new links

--- Bots_Pit/src/modules/Contact/Contact.tsx
+++ Bots_Pit/src/modules/Contact/Contact.tsx
@@ -1,10 +1,21 @@
 import "./Contact.css"
+import telegram from "../../images/icons8-telegram.svg"
 
 function Contact() {
     return (
         <div className="cont">
-            <a className="link" href="https://github.com/terracodum/Bots_Pit">Наша Почта</a>
-            <a className="link" href="https://github.com/terracodum/Bots_Pit">Наш НельзяГрам</a>
+            <p className="link">botspitgame@gmail.com</p>
+            <a className="link" href="https://t.me/botspitgame">
+            <div className="img"
+                style={{ width: '4rem', overflow: 'hidden' }}>
+                <img
+                    className="back"
+                    src={telegram} 
+                    alt="description" 
+                    style={{ width: '100%', height: '90%', zIndex: 1 }}
+                />
+            </div>
+            </a>
         </div>
     )
 }
--- Bots_Pit/src/modules/Download/Download.tsx
+++ Bots_Pit/src/modules/Download/Download.tsx
@@ -1,30 +1,25 @@
-import "./Download.css"
+import "./Download.css";
 import { useState } from 'react';
-
-function triggerDownload(fileName: string) {
-
-    var element = document.createElement('a');
-    element.setAttribute('href', fileName);
-    element.setAttribute('download', fileName);
-    element.style.display = 'none';
-    document.body.appendChild(element);
-    // Происходит клик, словно совершил его сам программирующий ниндзя
-    element.click();
-    document.body.removeChild(element);
-  }
 
 function Download() {
     const [isHovered, setIsHovered] = useState(false);
-    return(
+
+    const handleDownload = () => {
+        window.open('https://github.com/terracodum/Bots-Pit', '_blank');
+    };
+
+    return (
         <div className="btncont">
             <button 
-            onClick={() => {triggerDownload("Eblan.txt")}} 
-            onMouseEnter={() => setIsHovered(true)} // Устанавливаем состояние при наведении
-            onMouseLeave={() => setIsHovered(false)} // Убираем состояние при уходе
-            className={`down_btn highlight-container ${isHovered ? 'highlight' : ''}`}
-            >Скачать пре-альфу игры</button>
+                onClick={handleDownload} 
+                onMouseEnter={() => setIsHovered(true)} 
+                onMouseLeave={() => setIsHovered(false)} 
+                className={`down_btn highlight-container ${isHovered ? 'highlight' : ''}`}
+            >
+                Скачать пре-альфу игры
+            </button>
         </div>
-    )
+    );
 }
 
 export default Download;
